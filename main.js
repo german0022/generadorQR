741d3bdfa5136059c837be282c35082613ad855c
german0022 <71143584+german0022@users.noreply.github.com>
2023-08-22 01:03:06 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -3,25 +3,35 @@ button = document.getElementById("button"),
 input = document.getElementById("input"),
 qr = document.getElementById("qr"),
 img = document.getElementById("img");
-
+toast = document.getElementById("toast");
+close = document.getElementById("close");
 
 button.addEventListener("click", () => {
     let qrValue = input.value;
-    if(!qrValue) return;
-    img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrValue;
-    img.addEventListener("load", () =>{
-        container.classList.add("active");
-    });
+    if(!qrValue) {
+        toast.classList.add("active");
+    } else {
+        img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrValue;
+        img.addEventListener("load", () =>{
+            container.classList.add("active");
+            toast.classList.remove("active");
+        });
+    }
     
 });
 
 input.addEventListener("keyup", () => {
     if(!input.value) {
         container.classList.remove("active");
+        
     }
 });
 
 img.addEventListener("click", () => {
 
     window.open(img.src, "_blank");
+});
+
+close.addEventListener("click", () => {
+    toast.classList.remove("active");
 });
