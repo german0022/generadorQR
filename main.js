18ff114f26e75b343eae6bed7892117ec29a7d28
german0022 <71143584+german0022@users.noreply.github.com>
2023-08-21 06:37:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-const btnAdd = document.getElementById("btnAdd");
-const container = document.getElementById("container");
-const h1 = container.querySelector("h1");
-const inputs = document.getElementById("inputs");
+const container = document.querySelector(".container"),
+button = document.getElementById("button"),
+input = document.getElementById("input"),
+qr = document.getElementById("qr"),
+img = document.getElementById("img");
 
-btnAdd.addEventListener("click", function() {
-    container.classList.add("add");
-    btnAdd.style.display = ("none");
-    h1.classList.add("add");
-    inputs.classList.add("add");
+
+button.addEventListener("click", () => {
+    let qrValue = input.value;
+    if(!qrValue) return;
+    img.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrValue;
+    img.addEventListener("load", () =>{
+        container.classList.add("active");
+    });
+    
+});
+
+input.addEventListener("keyup", () => {
+    if(!input.value) {
+        container.classList.remove("active");
+    }
+});
+
+img.addEventListener("click", () => {
+
+    window.open(img.src, "_blank");
 });
